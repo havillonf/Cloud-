834f53b6be04904fac2feef93f13b0ca746351d6
havillonf <havillonbfreitas@gmail.com>
2024-04-14 18:59:11 -0300

    create enum for ageRating

---
+++ src/main/java/cloud/model/AgeRatingEnum.java
@@ -0,0 +1,17 @@
+package cloud.model;
+
+public enum AgeRatingEnum {
+    G("G"),
+    PG("PG"),
+    PG_13("PG-13"),
+    R("R"),
+    NC_17("NC-17"),
+    TV_MA("TV-MA"),
+    TV_14("TV-14"),
+    NOT_RATED("Not Rated"),
+    TV_G("TV-G"),
+    TV_PG("TV-PG");
+
+    AgeRatingEnum(String rating) {
+    }
+}
--- src/main/java/cloud/model/Movie.java
+++ src/main/java/cloud/model/Movie.java
@@ -23,7 +23,7 @@ public class Movie{
     private double rating;
     private String director;
     private String description;
-    private String ageRating;
+    private AgeRatingEnum ageRating;
     private String imgIdentification;
     @ManyToMany
     @JoinTable(
