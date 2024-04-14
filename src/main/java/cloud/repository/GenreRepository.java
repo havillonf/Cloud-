548acb48615fdc7f36a0eab18badf54525864e84
havillonf <havillonbfreitas@gmail.com>
2024-04-14 18:05:49 -0300

    adding list genres

---
+++ src/main/java/cloud/repository/GenreRepository.java
@@ -0,0 +1,8 @@
+package cloud.repository;
+
+import cloud.model.Genre;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GenreRepository extends JpaRepository<Genre, Long> {
+}
+
---
+++ src/main/java/cloud/service/GenreService.java
@@ -0,0 +1,19 @@
+package cloud.service;
+
+import cloud.model.Genre;
+import cloud.repository.GenreRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class GenreService {
+    private final GenreRepository genreRepository;
+
+    public List<Genre> getAllGenres() {
+        return genreRepository.findAll();
+    }
+
+}
